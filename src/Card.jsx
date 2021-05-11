ed2e70270b0672880c97347d957f843154e5e0cd
Christof Jens <christofjens@gmail.com>
2021-05-11 15:43:22 +0200

    Cleanup main

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,12 @@ import './Card.css'
 export default function Card({ headline, isBookmarkActive, text }) {
   return (
     <section className="Card">
-      <div className={isBookmarkActive ? 'bookmark active' : 'bookmark'}></div>
+      <button
+        className={
+          isBookmarkActive ? 'Card__bookmark active' : 'Card__bookmark'
+        }
+        aria-label="Bookmark card"
+      ></button>
       <h2>{headline}</h2>
       {text}
     </section>
