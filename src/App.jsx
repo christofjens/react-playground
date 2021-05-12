ac42839cc81f78edff60f108477551d368690a39
Christof Jens <christofjens@gmail.com>
2021-05-12 15:01:41 +0200

    Add todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import * as React from 'react'
-import Card from './Card'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './App.css'
 
 export default () => {
+  const [todos, setTodos] = useState([])
   return (
-    <div>
-      <Card
-        headline="This is a Headline"
-        isBookmarkActive
-        text="Featuring an active Bookmark. Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias incidunt asperiores."
-      ></Card>
-      <Card
-        headline="A second headline"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias incidunt asperiores."
-      ></Card>
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add Todo-Item:
+          <input name="todo" type="text" />
+        </label>
+        <button>Add</button>
+      </form>
+      <ul>
+        Todo-List
+        {todos.map(({ text, isDone, id }) => (
+          <li onClick={() => toggleIsDone(id)} key={id}>
+            {text} {isDone && '✅'}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+
+  function toggleIsDone(id) {
+    const index = todos.findIndex(todo => todo.id === id)
+    const todo = todos[index]
+
+    setTodos([
+      ...todos.slice(0, index),
+      { ...todo, isDone: !todo.isDone },
+      ...todos.slice(index + 1),
+    ])
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.todo
+    const newTodo = { text: input.value, isDone: false, id: uuidv4() }
+    setTodos([newTodo, ...todos])
+    form.reset()
+    input.focus()
+  }
 }
